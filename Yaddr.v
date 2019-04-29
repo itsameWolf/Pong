f56c6fd0a595838286181f560d3d351ce068c895
itsameWolf <44059936+itsameWolf@users.noreply.github.com>
2019-04-30 00:06:20 +0100

    added addreses counters

---
+++ Yaddr.v
@@ -0,0 +1,25 @@
+module Yaddr#(
+	parameter Y_MAX = 240
+	)(
+	input wire 			reset,
+	input wire 			add,
+	output reg [8:0] 	y_addr
+	);
+	
+	always @(posedge add or posedge reset) begin 
+		
+		if (reset) begin 
+			
+			y_addr <= 0;
+		
+		end else if (y_addr < Y_MAX) begin
+		
+			y_addr <= y_addr + 1;
+			
+		end else begin 
+		
+			y_addr <= 0;
+	
+	end
+	
+endmodule
